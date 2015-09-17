1c99c394a3a25bb2831487c4fcf1c4c976c2b652
vcernomschi <vcernomschi@vcernomschis-MacBook-Pro.local>
2015-09-17 11:52:48 +0300

    Add File Walker

--- src/test/Helpers/FileWalker.js
+++ src/test/Helpers/FileWalker.js
@@ -1,13 +1,18 @@
-// THIS TEST WAS GENERATED AUTOMATICALLY ON Thu Sep 03 2015 12:29:31 GMT+0300 (EEST)
-
 'use strict';
 
 import chai from 'chai';
 import {FileWalker} from '../../lib.compiled/Helpers/FileWalker';
 
-// @todo: Add more advanced tests
-suite("Helpers/FileWalker", function() {
+suite('Helpers/FileWalker', function() {
   test('Class FileWalker exists in Helpers/FileWalker', function() {
     chai.expect(typeof FileWalker).to.equal('function');
+  });
+
+  test('Check RECURSIVE static getter returns \'recursive\'', function() {
+    chai.expect(FileWalker.RECURSIVE).to.be.equal('recursive');
+  });
+
+  test('Check SIMPLE getter returns \'simple\'', function() {
+    chai.expect(FileWalker.SIMPLE).to.be.equal('simple');
   });
 });
