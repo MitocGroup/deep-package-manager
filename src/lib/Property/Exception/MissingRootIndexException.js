9cbca765e8e20a4cdcf7df1e151580a25126de66
mgoria <goria.marcel@gmail.com>
2015-09-17 11:43:43 +0300

    Fix 'no such file or directory '/tmp/src_1442477666279/ImageServing/Frontend/index.html' error

--- src/lib/Property/Exception/MissingRootIndexException.js
+++ src/lib/Property/Exception/MissingRootIndexException.js
@@ -2,7 +2,7 @@
  * Created by AlexanderC on 5/25/15.
  */
 
-"use strict";
+'use strict';
 
 import {Exception} from '../../Exception/Exception';
 
@@ -10,10 +10,10 @@ import {Exception} from '../../Exception/Exception';
  * throws when missing property root index.html file
  */
 export class MissingRootIndexException extends Exception {
-    /**
-     * @param {String} identifier
-     */
-    constructor(identifier) {
-        super(`Missing root property (${identifier}) index.html bootstrap file (application entry point)`);
-    }
+  /**
+   * @param {String} identifier
+   */
+  constructor(identifier) {
+    super(`Missing root property (${identifier}) index.html bootstrap file (application entry point)`);
+  }
 }
