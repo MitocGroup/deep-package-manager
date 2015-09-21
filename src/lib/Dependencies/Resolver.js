785dcefc421d2ad87522a074db669f98dd40fc33
vcernomschi <vcernomschi@vcernomschis-MacBook-Pro.local>
2015-09-21 15:03:40 +0300

    Add unit test for AbstractDriver

--- src/lib/Dependencies/Resolver.js
+++ src/lib/Dependencies/Resolver.js
@@ -13,79 +13,79 @@ import {Dispatcher} from './Dispatcher';
  * Dependencies resolver
  */
 export class Resolver extends Dispatcher {
-    /**
-     * @param {AbstractDriver} driver
-     */
-    constructor(driver) {
-        super(driver);
+  /**
+   * @param {AbstractDriver} driver
+   */
+  constructor(driver) {
+    super(driver);
 
-        this._resolveStack = [];
+    this._resolveStack = [];
+  }
+
+  /**
+   * @param {Microservice} microservice
+   * @param {Function} callback
+   */
+  dispatch(microservice, callback) {
+    this._pull(microservice.config.dependencies, function(pulledDependencies) {
+      let wait = new WaitFor();
+      let stackSize = 0;
+
+      pulledDependencies.forEach(function(dependencyPath) {
+        stackSize++;
+
+        this.dispatch(Microservice.create(dependencyPath), function() {
+          stackSize--;
+        }.bind(this));
+      }.bind(this));
+
+      wait.push(function() {
+        return stackSize <= 0;
+      }.bind(this));
+
+      wait.ready(function() {
+        callback();
+      }.bind(this));
+    }.bind(this));
+  }
+
+  /**
+   * @param {Object} dependencies
+   * @param {Function} callback
+   */
+  _pull(dependencies, callback) {
+    let wait = new WaitFor();
+    let stackSize = 0;
+    let pulledDependencies = [];
+
+    for (let dependencyName in dependencies) {
+      if (!dependencies.hasOwnProperty(dependencyName)) {
+        continue;
+      }
+
+      let dependencyVersion = dependencies[dependencyName];
+
+      if (this._resolveStack.indexOf(dependencyName) !== -1) {
+        //throw new InfiniteRecursionException(dependencyName, dependencyVersion);
+        continue;
+      }
+
+      stackSize++;
+      this._resolveStack.push(dependencyName);
+
+      this._driver.pull(dependencyName, dependencyVersion, function(outputPath) {
+        pulledDependencies.push(outputPath);
+
+        stackSize--;
+      }.bind(this));
     }
 
-    /**
-     * @param {Microservice} microservice
-     * @param {Function} callback
-     */
-    dispatch(microservice, callback) {
-        this._pull(microservice.config.dependencies, function(pulledDependencies) {
-            let wait = new WaitFor();
-            let stackSize = 0;
+    wait.push(function() {
+      return stackSize <= 0;
+    }.bind(this));
 
-            pulledDependencies.forEach(function(dependencyPath) {
-                stackSize++;
-
-                this.dispatch(Microservice.create(dependencyPath), function() {
-                    stackSize--;
-                }.bind(this));
-            }.bind(this));
-
-            wait.push(function() {
-                return stackSize <= 0;
-            }.bind(this));
-
-            wait.ready(function() {
-                callback();
-            }.bind(this));
-        }.bind(this));
-    }
-
-    /**
-     * @param {Object} dependencies
-     * @param {Function} callback
-     */
-    _pull(dependencies, callback) {
-        let wait = new WaitFor();
-        let stackSize = 0;
-        let pulledDependencies = [];
-
-        for(let dependencyName in dependencies) {
-            if (!dependencies.hasOwnProperty(dependencyName)) {
-                continue;
-            }
-
-            let dependencyVersion = dependencies[dependencyName];
-
-            if (this._resolveStack.indexOf(dependencyName) !== -1) {
-                //throw new InfiniteRecursionException(dependencyName, dependencyVersion);
-                continue;
-            }
-
-            stackSize++;
-            this._resolveStack.push(dependencyName);
-
-            this._driver.pull(dependencyName, dependencyVersion, function(outputPath) {
-                pulledDependencies.push(outputPath);
-
-                stackSize--;
-            }.bind(this));
-        }
-
-        wait.push(function() {
-            return stackSize <= 0;
-        }.bind(this));
-
-        wait.ready(function() {
-            callback(pulledDependencies);
-        }.bind(this));
-    }
+    wait.ready(function() {
+      callback(pulledDependencies);
+    }.bind(this));
+  }
 }
