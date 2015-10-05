34666471b14a123a0de039fa468dcb6db4a5379a
Stefan <hariton.stefan@outlook.com>
2015-10-05 14:46:51 +0300

    Unit tests for exceptions

---
+++ src/test/Property/Exception/FailedUploadingFileToS3Exception.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Stefan Hariton on 10/5/15.
+ */
+
+"use strict";
+
+import chai from 'chai'
+import {FailedUploadingFileToS3Exception} from '../../../lib.compiled/Property/Exception/FailedUploadingFileToS3Exception';
+
+suite('Property/Exception/FailedUploadingFileToS3Exception', function() {
+
+  test('Class FailedUploadingFileToS3Exception', function () {
+    let e = new FailedUploadingFileToS3Exception('Test exception');
+    chai.expect(e).to.be.an.instanceof(FailedUploadingFileToS3Exception);
+  });
+});
