2ccb21076a32739286e8b184da594b08f1bb1a71
AlexanderC <alexander.moldova@gmail.com>
2015-11-19 15:30:15 +0200

    Improve export

--- src/lib/bootstrap.js
+++ src/lib/bootstrap.js
@@ -6,9 +6,32 @@
 
 'use strict';
 
-import {Instance as Property} from './Property/Instance';
+import {FileWalker} from './Helpers/FileWalker';
+import path from 'path';
 
-// @todo: think on exporting more objects
-var exports = module.exports = {
-    Property: Property
-};
+let walker = new FileWalker(FileWalker.RECURSIVE);
+
+let classFiles = walker.walk(__dirname, FileWalker.skipDotsFilter((file) => {
+  return /^(.*\/)?[A-Z][^\/]+\.js$/.test(file);
+})).map((file) => file.substr(__dirname.length));
+
+let exp = {};
+
+classFiles.forEach((classFile) => {
+  let matches = classFile.match(/^(?:.*\/)?([A-Z][^\/]+)\.js$/);
+  let className = matches[1];
+
+  let nsParts = classFile.split('/').filter((part) => !!part);
+  nsParts.pop();
+
+  let jsObj = require(path.join(__dirname, classFile));
+
+  if (jsObj.hasOwnProperty(className)) {
+    let classObj = jsObj[className];
+    nsParts.push(className);
+
+    exp[nsParts.join('_')] = classObj;
+  }
+});
+
+export default exp;
