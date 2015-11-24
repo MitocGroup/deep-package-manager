cc331fcc1b7ea0fa9234cd7bd412d42eb5e73a7c
vcernomschi <vcernomschi@mitocgroup.com>
2015-11-24 19:15:26 +0200

    Add unit tests to Replacer

--- src/test/Assets/Replacer/AbstractReplacer.js
+++ src/test/Assets/Replacer/AbstractReplacer.js
@@ -1,13 +1,26 @@
-// THIS TEST WAS GENERATED AUTOMATICALLY ON Tue Nov 24 2015 11:39:59 GMT+0200 (EET)
-
 'use strict';
 
 import chai from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
 import {AbstractReplacer} from '../../../lib.compiled/Assets/Replacer/AbstractReplacer';
+import {AbstractReplacerMock} from '../../mock/Assets/Replacer/AbstractReplacerMock'
 
-// @todo: Add more advanced tests
-suite("Assets/Replacer/AbstractReplacer", function() {
-  test('Class AbstractReplacer exists in Assets/Replacer/AbstractReplacer', function() {
+chai.use(sinonChai);
+
+suite('Assets/Replacer/AbstractReplacer', function () {
+  let version = 'test_version';
+  let abstractReplacer = new AbstractReplacerMock(version);
+
+  test('Class AbstractReplacer exists in Assets/Replacer/AbstractReplacer', function () {
     chai.expect(typeof AbstractReplacer).to.equal('function');
+  });
+
+  test('Check constructor sets version', function () {
+    chai.expect(abstractReplacer.version).to.equal(version);
+  });
+
+  test('Check replace() method', function () {
+    chai.expect(abstractReplacer.replace().isReplaced).to.equal(true);
   });
 });
--- src/test/Assets/Replacer/UrlReplacer.js
+++ src/test/Assets/Replacer/UrlReplacer.js
@@ -1,13 +1,58 @@
-// THIS TEST WAS GENERATED AUTOMATICALLY ON Tue Nov 24 2015 11:39:59 GMT+0200 (EET)
-
 'use strict';
 
 import chai from 'chai';
 import {UrlReplacer} from '../../../lib.compiled/Assets/Replacer/UrlReplacer';
 
-// @todo: Add more advanced tests
-suite("Assets/Replacer/UrlReplacer", function() {
-  test('Class UrlReplacer exists in Assets/Replacer/UrlReplacer', function() {
+suite('Assets/Replacer/UrlReplacer', function () {
+  let version = 'test_version';
+  let urlReplacer = new UrlReplacer(version);
+
+  test('Class UrlReplacer exists in Assets/Replacer/UrlReplacer', function () {
     chai.expect(typeof UrlReplacer).to.equal('function');
+  });
+
+  test('Check constructor sets version', function () {
+    chai.expect(urlReplacer.version).to.equal(version);
+  });
+
+  test('Check _getUriDelimiter() returns "&"', function () {
+    let uri = 'http://host.com/p/a/t/h?query=string#hash'
+    chai.expect(UrlReplacer._getUriDelimiter(uri)).to.equal('&');
+  });
+
+  test('Check _getUriDelimiter() returns "?"', function () {
+    let uri = 'http://host.com/p/a/t/h/'
+    chai.expect(UrlReplacer._getUriDelimiter(uri)).to.equal('?');
+  });
+
+  test('Check _replaceAll() replaces all search in string and returns result', function () {
+    let testStr = 'here test - simple text test will be replaced to tesT';
+    let search = 'test';
+    let replace = 'teST';
+    let expectedResult = 'here teST - simple text teST will be replaced to tesT';
+
+    let actualResult = UrlReplacer._replaceAll(testStr, search, replace);
+
+    chai.expect(actualResult).to.equal(expectedResult);
+  });
+
+  test('Check _parseHtml() returns valid array', function () {
+    let content = '<body>' +
+      '<img src="../foo.jpg">' +
+      '<p>hi</p>' +
+      '<script src="scripts/x.js"></script>' +
+      '<script src="scripts/y.js"></script>' +
+      '<link rel=stylesheet href="/styles/thing.css">' +
+      '</body>';
+    let expectedResult = {
+      '<img src="../foo.jpg">': '../foo.jpg',
+      '<link rel=stylesheet href="/styles/thing.css">': '/styles/thing.css',
+      '<script src="scripts/x.js"></script>': 'scripts/x.js',
+      '<script src="scripts/y.js"></script>': 'scripts/y.js',
+    };
+
+    let actualResult = UrlReplacer._parseHtml(content);
+
+    chai.expect(actualResult).to.eql(expectedResult);
   });
 });
