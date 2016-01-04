c178913c196380b993d2340166c7d0461326f8a1
vcernomschi <vcernomschi@mitocgroup.com>
2016-01-04 23:20:33 +0200

    Debug send coverage report

--- test/bin/coverage.sh
+++ test/bin/coverage.sh
@@ -4,7 +4,7 @@ source $(dirname $0)/_head.sh
 
 ### Upload Coverage info to Codacy ###
 
-cat ${__SRC_PATH}"/coverage/lcov.info" | codacy-coverage
+cat ${__SRC_PATH}"/coverage/lcov.info" | codacy-coverage --debug
 
 ### Log top 20 file paths to be able see paths format from travis###
 head -n 20 ${__SRC_PATH}"/coverage/lcov.info"
