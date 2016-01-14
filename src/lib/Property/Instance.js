e095c98c1493d66f773f51382516a847e6a1c1e8
AlexanderC <alexander.moldova@gmail.com>
2016-01-14 18:38:33 +0200

    Fix deployId

--- src/lib/Property/Instance.js
+++ src/lib/Property/Instance.js
@@ -56,6 +56,8 @@ export class Instance {
     this._isUpdate = false;
 
     this._microservicesToUpdate = [];
+
+    this._config.deployId = new DeployID(this).toString();
   }
 
   /**
@@ -154,10 +156,6 @@ export class Instance {
    * @returns {String}
    */
   get deployId() {
-    if (!this._config.deployId) {
-      this._config.deployId = new DeployID(this).toString();
-    }
-
     return this._config.deployId;
   }
 
@@ -742,10 +740,12 @@ export class Instance {
   update(propertyConfigSnapshot, callback, microservicesToUpdate = []) {
     this._isUpdate = true;
     this.microservicesToUpdate = microservicesToUpdate;
-    this._config = propertyConfigSnapshot;
 
-    // @todo: remove it?
-    this._config.deployId = null;
+    // keep initial deployId
+    let deployId = this.deployId;
+
+    this._config = propertyConfigSnapshot;
+    this._config.deployId = deployId;
 
     this._provisioning.injectConfig(
       this._config.provisioning
