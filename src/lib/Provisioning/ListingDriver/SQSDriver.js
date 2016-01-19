e57232b0f3d1657b74080eaeebbeedc96d9aafa0
mgoria <goria.marcel@gmail.com>
2016-01-19 18:57:38 +0200

    #107 - Add SQS implementation for listing and undeploy

---
+++ src/lib/Provisioning/ListingDriver/SQSDriver.js
@@ -0,0 +1,50 @@
+/**
+ * Created by mgoria on 01/19/16.
+ */
+
+'use strict';
+
+import {AbstractDriver} from './AbstractDriver';
+
+export class SQSDriver extends AbstractDriver {
+  /**
+   * @param {*} args
+   */
+  constructor(...args) {
+    super(...args);
+  }
+
+  /**
+   * @param {Function} cb
+   */
+  list(cb) {
+    this.awsService.listQueues({
+      QueueNamePrefix: SQSDriver.QUEUE_NAME_PREFIX,
+    }, (error, data) => {
+      if (error) {
+        cb(error);
+        return;
+      }
+
+      for (let i in data.QueueUrls) {
+        if (!data.QueueUrls.hasOwnProperty(i)) {
+          continue;
+        }
+
+        let queueUrl = data.QueueUrls[i];
+        let queueName = queueUrl.split('/').pop();
+
+        this._checkPushStack(queueName, queueUrl, queueUrl);
+      }
+
+      cb(null);
+    });
+  }
+
+  /**
+   * @returns {Number}
+   */
+  static get QUEUE_NAME_PREFIX() {
+    return 'Deep';
+  }
+}
