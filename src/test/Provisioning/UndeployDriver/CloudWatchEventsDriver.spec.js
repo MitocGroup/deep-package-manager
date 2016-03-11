8772120742fa0685b8ff23f43ef69c8efd4b2074
AlexanderC <alexander.moldova@gmail.com>
2016-03-11 13:31:20 +0200

    Generate missing tests

---
+++ src/test/Provisioning/UndeployDriver/CloudWatchEventsDriver.spec.js
@@ -0,0 +1,13 @@
+// THIS TEST WAS GENERATED AUTOMATICALLY ON Fri Mar 11 2016 13:31:10 GMT+0200 (EET)
+
+'use strict';
+
+import chai from 'chai';
+import {CloudWatchEventsDriver} from '../../../lib/Provisioning/UndeployDriver/CloudWatchEventsDriver';
+
+// @todo: Add more advanced tests
+suite('Provisioning/UndeployDriver/CloudWatchEventsDriver', function() {
+  test('Class CloudWatchEventsDriver exists in Provisioning/UndeployDriver/CloudWatchEventsDriver', () => {
+    chai.expect(CloudWatchEventsDriver).to.be.an('function');
+  });
+});
---
+++ src/test/Provisioning/UndeployDriver/ESDriver.spec.js
@@ -0,0 +1,13 @@
+// THIS TEST WAS GENERATED AUTOMATICALLY ON Fri Mar 11 2016 13:31:10 GMT+0200 (EET)
+
+'use strict';
+
+import chai from 'chai';
+import {ESDriver} from '../../../lib/Provisioning/UndeployDriver/ESDriver';
+
+// @todo: Add more advanced tests
+suite('Provisioning/UndeployDriver/ESDriver', function() {
+  test('Class ESDriver exists in Provisioning/UndeployDriver/ESDriver', () => {
+    chai.expect(ESDriver).to.be.an('function');
+  });
+});
