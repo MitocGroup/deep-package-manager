d5d1f8410890b82e301616f707b6924d5e8dfb50
mgoria <goria.marcel@gmail.com>
2016-03-28 15:27:10 +0300

    #225: Add list OIDC Providers in IAM listing driver

--- src/lib/Provisioning/ListingDriver/IAMDriver.js
+++ src/lib/Provisioning/ListingDriver/IAMDriver.js
@@ -18,6 +18,30 @@ export class IAMDriver extends AbstractDriver {
    * @param {Function} cb
    */
   list(cb) {
+    let methods = ['_listRoles', '_listOIDCProviders'];
+    let responseCount = 0;
+    let errors = [];
+
+    methods.forEach((methodName) => {
+      this[methodName]((error) => {
+        responseCount++;
+
+        if (error) {
+          errors.push(error);
+        }
+
+        if (responseCount === methods.length) {
+          cb(errors.length > 0 ? errors.join('; ') : null);
+        }
+      });
+    });
+  }
+
+  /**
+   * @param {Function} cb
+   * @private
+   */
+  _listRoles(cb) {
     this._awsService.listRoles({
       MaxItems: IAMDriver.MAX_ITEMS,
     }, (error, data) => {
@@ -36,6 +60,27 @@ export class IAMDriver extends AbstractDriver {
 
         this._checkPushStack(roleName, roleName, roleData);
       }
+
+      cb(null);
+    });
+  }
+
+  /**
+   * @param {Function} cb
+   * @private
+   */
+  _listOIDCProviders(cb) {
+    this._awsService.listOpenIDConnectProviders({}, (error, data) => {
+      if (error) {
+        cb(error);
+        return;
+      }
+
+      data.OpenIDConnectProviderList.forEach((provider) => {
+        let providerARN = provider.Arn;
+
+        this._checkPushStack(providerARN, providerARN, providerARN);
+      });
 
       cb(null);
     });
