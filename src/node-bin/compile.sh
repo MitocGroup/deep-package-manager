7d824d8e4855700ebb975d20ead1376c6d0a3ede
vcernomschi <vcernomschi@mitocgroup.com>
2016-05-26 15:56:02 +0300

    Fix coverage to es6

--- src/node-bin/compile.sh
+++ src/node-bin/compile.sh
@@ -1,12 +1,33 @@
-if [ "$TRAVIS" == "true" ]; then
-    echo "TravisCI environment detected. Skipping code transpiling to ES5..."
-elif [ -d 'lib/' ] && [ "$OSTYPE" != "win32" ] && [ "$OSTYPE" != "win64" ]; then
-    NPM_GLOBAL_NM=`npm root -g`;
+#!/usr/bin/env bash
+#
+# Created by vcernomschi on 24/05/2016
+#
 
-    BABEL_ENV=production babel lib/ --out-dir lib.compiled/ --presets ${NPM_GLOBAL_NM}/babel-preset-es2015 \
-        --plugins ${NPM_GLOBAL_NM}/babel-plugin-add-module-exports;
+if [ "$TRAVIS" == "true" ]; then
+
+  ######################################################
+  ### Skip transpile to ES5 when running from Travis ###
+  ######################################################
+  echo "TravisCI environment detected. Skipping code transpiling to ES5..."
+elif [ -d 'lib/' ] && [ "$OSTYPE" != "win32" ] && [ "$OSTYPE" != "win64" ]; then
+
+  #################################################################
+  ### Transpile to ES5 when running on local and not on Windows ###
+  #################################################################
+  NPM_GLOBAL_NM=`npm root -g`;
+
+  BABEL_ENV=production babel lib/ --out-dir lib.compiled/ --presets ${NPM_GLOBAL_NM}/babel-preset-es2015 \
+    --plugins ${NPM_GLOBAL_NM}/babel-plugin-add-module-exports;
 elif [ "$OSTYPE" == "win32" ] || [ "$OSTYPE" == "win64" ]; then
-   echo "You should have installed and configured http://git-scm.com/ and run all bash command by using git-bash.exe"
+
+  #####################################################
+  ### Skip transpiling on Windows from command line ###
+  #####################################################
+  echo "You should have installed and configured http://git-scm.com/ and run all bash command by using git-bash.exe"
 else
-   echo "Skipping code transpiling to ES5..."
+
+  ######################################################
+  ### Skip transpiling if `lib` folder doesn't exist ###
+  ######################################################
+  echo "Skipping code transpiling to ES5..."
 fi
--- src/node-bin/test.sh
+++ src/node-bin/test.sh
@@ -1,24 +1,33 @@
+#!/usr/bin/env bash
+#
+# Created by vcernomschi on 24/05/2016
+#
+
 if [ -d 'lib/' ] && [ "$OSTYPE" != "msys" ] && [ "$OSTYPE" != "win32" ] && [ "$OSTYPE" != "win64" ]; then
-    COMPILE_DIR='./compile';
-    [ -d ${COMPILE_DIR} ] && rm -rf ${COMPILE_DIR};
 
-    COMPILE() {
-        local resource=$1;
-
-        babel -x .js --presets es2015 --plugins add-module-exports \
-            ${resource} --out-dir ${COMPILE_DIR}/${resource}
-    }
-
-    COMPILE lib;
-    COMPILE test;
-
-    isparta cover --include ${COMPILE_DIR}/lib/**/*.js `which _mocha` -- ${COMPILE_DIR}/test/**/*.spec.js \
-        --reporter spec --ui tdd --recursive --timeout 10s
+ #########################################################################
+ ### Run with babel-node to support ES6 tests and have coverage in ES6 ###
+ #########################################################################
+ babel-node $(npm root -g)/istanbul/lib/cli.js cover `which _mocha` -- 'test/**/*.spec.js' \
+ --reporter spec --ui tdd --recursive --timeout 20s
 elif [ "$OSTYPE" == "win32" ] || [ "$OSTYPE" == "win64" ]; then
-    echo "You should have installed and configured http://git-scm.com/ and run all bash command by using git-bash.exe"
+
+ #################################################
+ ### Skip running on Windows from command line ###
+ #################################################
+ echo "You should have installed and configured http://git-scm.com/ and run all bash command by using git-bash.exe"
 elif [ -d 'lib/' ]; then
-    echo "Running from git-bash without gathering coverage"
-    babel-node `which _mocha` --ui tdd --recursive --reporter spec
+
+ #########################################
+ ### Running from git-bash on Windows  ###
+ #########################################
+ echo "Running from git-bash with gathering coverage"
+ babel-node $(npm root -g)/istanbul/lib/cli.js cover `which _mocha` -- 'test/**/*.spec.js' \
+ --reporter spec --ui tdd --recursive --timeout 20s
 else
-   echo "Skipping testing..."
+
+ ##################################################
+ ### Skip running if `lib` folder doesn't exist ###
+ ##################################################
+ echo "Skipping testing..."
 fi
