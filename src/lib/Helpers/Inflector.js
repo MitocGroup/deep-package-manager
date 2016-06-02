3d389f5742ad3c7aa0d82d8791f98e65c3a06124
Cristi <covali.christian@gmail.com>
2016-06-02 14:58:25 +0300

    # Switch from pascal-case to lisp-case (#287)
    * # Switch from pascal-case to lisp-case
    
    * # Switch from pascal-case to lisp-case
    
    * # Switch from pascal-case to lisp-case

--- src/lib/Helpers/Inflector.js
+++ src/lib/Helpers/Inflector.js
@@ -22,14 +22,26 @@ export class Inflector {
   }
 
   /**
-   * @param {String} string
+   * @param {String} str
    * @returns {String}
    */
-  static pascalCase(string) {
-    return string
+  static pascalCase(str) {
+    return str
       .split(/[^a-zA-Z0-9]+/)
       .reduce((pascalString, part) => {
         return pascalString + Inflector.capitalizeFirst(part);
       }, '');
+  }
+
+  /**
+   * @param {String} str
+   * @returns {*}
+   */
+  static lispCase(str) {
+    return str
+      .replace(/([a-z])([A-Z])/g, '$1-$2')
+      .split(/[^a-z0-9\-]+/i)
+      .join('-')
+      .toLowerCase();
   }
 }
