e4be9c3220db7d62f30c5f65d0b6174dc51b46b5
CCristi <covali.christian@gmail.com>
2016-07-01 11:55:22 +0300

    Merge branch 'dev' of github.com:MitocGroup/deep-package-manager into dev

--- src/lib/Property/ExtractStrategy/OptimisticStrategy.js
+++ src/lib/Property/ExtractStrategy/OptimisticStrategy.js
@@ -7,8 +7,6 @@
 import {AbstractStrategy} from './AbstractStrategy';
 import Path from 'path';
 import {Config} from '../Config';
-import {Instance as Microservice} from '../../Microservice/Instance';
-import {MissingRootException} from '../Exception/MissingRootException';
 import fs from 'fs';
 import fse from 'fs-extra';
 
@@ -36,37 +34,10 @@ export class OptimisticStrategy extends AbstractStrategy {
     let configFile = Path.join(this._path, Config.DEFAULT_FILENAME);
 
     if (!fs.existsSync(configFile)) {
-      if (this._hasMicroservices()) {
-        fse.outputJsonSync(configFile, Config.generate());
-      } else {
-        throw new MissingRootException();
-      }
+      fse.outputJsonSync(configFile, Config.generate());
     }
 
     return fse.readJsonSync(configFile);
-  }
-
-  /**
-   * @param {String|null} path
-   * @returns {Boolean}
-   * @private
-   */
-  _hasMicroservices(path = null) {
-    path = path || this._path;
-
-    let resources = fs.readdirSync(path);
-
-    for (let resource of resources) {
-      let fullPath = Path.join(path, resource);
-
-      if (fs.statSync(fullPath).isDirectory() &&
-        fs.existsSync(Path.join(fullPath, Microservice.CONFIG_FILE))) {
-
-        return true;
-      }
-    }
-
-    return false;
   }
 
   /**
