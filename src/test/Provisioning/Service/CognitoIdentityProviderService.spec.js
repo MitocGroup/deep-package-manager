a9d69ca5348ca44524b9285d7018dcf5ea0d52e5
CCristi <covali.christian@gmail.com>
2016-07-28 19:05:31 +0300

    # Added test for generateAllowActionStatement

--- src/test/Provisioning/Service/CognitoIdentityProviderService.spec.js
+++ src/test/Provisioning/Service/CognitoIdentityProviderService.spec.js
@@ -3,11 +3,23 @@
 'use strict';
 
 import chai from 'chai';
+import Core from 'deep-core';
 import {CognitoIdentityProviderService} from '../../../lib/Provisioning/Service/CognitoIdentityProviderService';
+import {ProvisioningInstanceMock} from '../../mock/Provisioning/ProvisioningInstanceMock';
 
 // @todo: Add more advanced tests
 suite('Provisioning/Service/CognitoIdentityProviderService', function() {
   test('Class CognitoIdentityProviderService exists in Provisioning/Service/CognitoIdentityProviderService', () => {
     chai.expect(CognitoIdentityProviderService).to.be.an('function');
+  });
+
+  let propertyInstance = new PropertyInstanceMock('./test/testMaterials/Property2', 'deeploy.test.json');
+  let provisioningInstance = new ProvisioningInstanceMock(propertyInstance);
+  let cognitoIdpService = new CognitoIdentityProviderService(provisioningInstance);
+
+  test('Check "generateAllowActionsStatement" returns in instanceof Core.AWS.IAM.Statement', () => {
+    let statement = cognitoIdpService.generateAllowActionsStatement(['testAction']);
+
+    chai.expect(statement).to.be.an.instanceof(Core.AWS.IAM.Statement);
   });
 });
