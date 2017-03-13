a6842268ba172db78196d5f6cab3705a33ac949b
CCristi <covali.christian@gmail.com>
2017-03-13 15:18:52 +0200

    #Fix CloudFront undeploy and listing

--- src/lib/Provisioning/ResourceTagging/Driver/AbstractDriver.js
+++ src/lib/Provisioning/ResourceTagging/Driver/AbstractDriver.js
@@ -65,15 +65,15 @@ export class AbstractDriver extends Core.OOP.Interface {
    * @returns {{DeepApplicationId: String, DeepDeployId: String, DeepEnvironmentId: *, DeepEnvironmentName: String}}
    */
   get tags() {
-    let payload = {
-      DeepApplicationId: this._property.identifier,
-      DeepDeployId: this._property.deployId,
-      DeepEnvironmentId: this._envId,
-      DeepEnvironmentName: this._property.env,
-    };
+    let payload = {};
+
+    payload[AbstractDriver.APPLICATION_ID_KEY] = this._property.identifier;
+    payload[AbstractDriver.DEPLOY_ID_KEY] = this._property.deployId;
+    payload[AbstractDriver.ENVIRONMENT_ID_KEY] = this._envId;
+    payload[AbstractDriver.ENVIRONMENT_NAME_KEY] = this._property.env;
 
     if (this._applicationName) {
-      payload.DeepApplicationName = this._applicationName;
+      payload[AbstractDriver.APPLICATION_NAME_KEY] = this._applicationName;
     }
 
     return payload;
@@ -87,5 +87,41 @@ export class AbstractDriver extends Core.OOP.Interface {
       this._property.config.awsAccountId,
       this._property.identifier
     );
+  }
+
+  /**
+   * @returns {String}
+   */
+  static get DEPLOY_ID_KEY() {
+    return 'DeepDeployId';
+  }
+
+
+  /**
+   * @returns {String}
+   */
+  static get APPLICATION_NAME_KEY() {
+    return 'DeepApplicationName';
+  }
+
+  /**
+   * @returns {String}
+   */
+  static get APPLICATION_ID_KEY() {
+    return 'DeepApplicationId';
+  }
+
+  /**
+   * @returns {String}
+   */
+  static get ENVIRONMENT_NAME_KEY() {
+    return 'DeepEnvironmentName';
+  }
+
+  /**
+   * @returns {String}
+   */
+  static get ENVIRONMENT_ID_KEY() {
+    return 'DeepEnvironmentId';
   }
 }
