2850db600a06a7f7f867c28fc39034f597078825
AlexanderC <alexander.moldova@gmail.com>
2017-04-12 16:28:31 +0300

    Fix module export

---
+++ src/node-bin/bootstrap-dist.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const path = require('path');
+const FileWalker = require('../lib.compiled/Helpers/FileWalker').FileWalker;
+
+const modules = {};
+const libsDir = path.resolve(__dirname, '..', 'lib.compiled');
+const walker = new FileWalker(FileWalker.RECURSIVE);
+
+walker.walk(
+  libsDir, 
+  FileWalker.skipDotsFilter(file => {
+    return /^(.*[\/|\\])?[A-Z][^\/\\]+\.js$/.test(file);
+  })
+)
+.map(file => file.substr(libsDir.length + 1))
+.map(classFile => {
+  const matches = classFile.match(/^(?:.*[\/|\\])?([A-Z][^\/\\]+)\.js$/);
+  const className = matches[1];
+  const nsParts = classFile.split(path.sep).filter(part => !!part);
+  
+  nsParts.pop();
+
+  const jsFile = path.resolve(libsDir, classFile);
+  const jsObj = require(jsFile);
+
+  if (jsObj.hasOwnProperty(className)) {
+    let classObj = jsObj[className];
+    nsParts.push(className);
+    
+    modules[nsParts.join('_')] = `require('${jsFile}').${className}`;
+  }
+});
+
+process.stdout.write(`'use strict';\n`);
+process.stdout.write(`module.exports = {};\n`);
+
+Object.keys(modules).map(moduleKey => {
+  process.stdout.write(`module.exports.${moduleKey} = ${modules[moduleKey]};\n`);
+});
